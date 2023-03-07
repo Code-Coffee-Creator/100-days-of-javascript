8f80e37b992999f9e5c202a0453b05b68934d4ec
Denny Pearce <code.coffee.creator@gmail.com>
2023-03-07 14:15:55 +0200

    add day-11, day-12, day-13, day-14

--- day-11/app.js
+++ day-11/app.js
@@ -1,3 +1,27 @@
+// CREATE A QUIZ CLASS
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questionIndex = 0;
+    this.questions = questions;
+  }
+
+  getQuestionIndex() {
+    return this.questions[this.questionIndex];
+  }
+
+  guess(answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.questionIndex++;
+  }
+
+  isEnded() {
+    return this.questionIndex === this.questions.length;
+  }
+}
+
 class Question {
   constructor(text, choices, answer) {
     this.text = text;
@@ -8,6 +32,54 @@ class Question {
   isCorrectAnswer(choice) {
     return this.answer === choice;
   }
+}
+
+// DISPLAY THE QUESTIONS
+function displayQuestion() {
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    let questionElement = document.getElementById('question');
+    questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+    let choices = quiz.getQuestionIndex().choices;
+    for (let i = 0; i < choices.length; i++) {
+      let choiceElement = document.getElementById('choice' + i);
+      choiceElement.innerHTML = choices[i];
+      guess('btn' + i, choices[i]);
+    }
+
+    showProgress();
+  }
+}
+
+// GUESS ANSWER
+function guess(id, guess) {
+  let button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    displayQuestion();
+  };
+}
+
+// SHOW QUIZ PROGRESS
+function showProgress() {
+  let currentQuestionNumber = quiz.questionIndex + 1;
+  let progressElement = document.getElementById('progress');
+  progressElement.innerHTML = `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+}
+
+// SHOW SCORES
+function showScores() {
+  let quizEndHTML = `
+    <h1>Quiz Completed</h1>
+    <h2 id='score'> Your scored: ${quiz.score} of ${quiz.questions.length}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Take Quiz Again</a>
+    </div>
+  `;
+  let quizElement = document.getElementById('quiz');
+  quizElement.innerHTML = quizEndHTML;
 }
 
 // create questions here
@@ -38,3 +110,30 @@ let questions = [
     'Python'
   ),
 ];
+
+let quiz = new Quiz(questions);
+
+displayQuestion();
+
+// add a countdown for the quiz
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById('count-down');
+
+function startCountdown() {
+  let quizTimer = setInterval(() => {
+    if (quizTime <= 0) {
+      clearInterval(quizTimer);
+      showScores();
+    } else {
+      quizTime--;
+      let sec = Math.floor(quizTime % 60);
+      let min = Math.floor(quizTime / 60) % 60;
+      counting.innerHTML = `TIME: ${min} : ${sec}`;
+    }
+  }, 1000);
+}
+
+startCountdown();
---
+++ day-14/ajax.js
@@ -0,0 +1,51 @@
+const btn = document.querySelector('.get-quotes');
+btn.addEventListener('click', getQuotes);
+const number = document.getElementById('number');
+
+function getQuotes(e) {
+  e.preventDefault();
+  if (number.value.length == 0) {
+    return alert('Please enter a number');
+  } else {
+    const https = new XMLHttpRequest();
+    https.open('GET', 'https://type.fit/api/quotes', true);
+    https.onload = function () {
+      if (this.status === 200) {
+        const response = shuffle(JSON.parse(this.responseText));
+
+        let output = '';
+
+        for (let i = 0; i < response.length; i++) {
+          if (i == number.value) {
+            break;
+          }
+          output += `
+            <li>Quote: ${response[i].text}</li>
+            <li>Author: ${response[i].author}</li>
+            <hr>
+          `;
+        }
+
+        document.querySelector('.quotes').innerHTML = output;
+      }
+    };
+
+    https.send();
+  }
+}
+
+function shuffle(quotes) {
+  let CI = quotes.length,
+    tempValue,
+    randomIndex;
+
+  while (CI > 0) {
+    randomIndex = Math.floor(Math.random() * CI);
+    CI--;
+
+    tempValue = quotes[CI];
+    quotes[CI] = quotes[randomIndex];
+    quotes[randomIndex] = tempValue;
+  }
+  return quotes;
+}
